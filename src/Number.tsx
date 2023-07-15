7822c3eee5d68400f6baff3d97297f92021c37be
Peter <sb1285n@gmail.com>
2023-07-15 11:29:06 -0400

    chore(package.json): add new dependencies for Pixi.js filters and React bindings feat(App.tsx): implement clock face with numbers and clock hands feat(ClockHand.tsx): create ClockHand component to draw clock hands feat(Number.tsx): create Number component to display numbers on clock face
    chore(logo.svg): delete logo.svg file

---
+++ src/Number.tsx
@@ -0,0 +1,37 @@
+import { Text } from "@pixi/react";
+import * as PIXI from "pixi.js";
+
+interface NumberProps {
+  x: number;
+  y: number;
+  text: string;
+}
+
+const Number: React.FC<NumberProps> = ({ x, y, text }) => {
+  return (
+    <Text
+      text={text}
+      anchor={0.5}
+      x={x}
+      y={y}
+      style={
+        new PIXI.TextStyle({
+          align: "center",
+          fontFamily: '"Segoe UI", Tahoma, Geneva, Verdana, sans-serif',
+          fontSize: 48,
+          fontWeight: "bold",
+          fill: "#ffffff",
+          stroke: "#000000",
+          strokeThickness: 2,
+          dropShadow: true,
+          dropShadowColor: "#7a7a7a",
+          dropShadowBlur: 1,
+          dropShadowAngle: Math.PI,
+          dropShadowDistance: 0,
+        })
+      }
+    />
+  );
+};
+
+export default Number;
